7c3462dc24dc94a620bb4d67004361f263b37c1d
Griotold <gotjd9773@naver.com>
2023-02-03 11:58:15 +0900

    기본 문법 - 조인, 세타조인

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -219,4 +219,45 @@ public class QuerydslBasicTest {
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
 
     }
+    /**
+     * join 연습
+     * 팀 A에 소속된 모든 회원
+     *
+     * */
+    @Test
+    public void joinTest() throws Exception {
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .join(member.team, team) // Qteam.team 인데 스태틱 임포트
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+    /**
+     * 세타 조인(연관관계가 없는 필드로 조인)
+     * 회원의 이름이 팀 이름과 같은 회원 조회
+     * */
+    @Test
+    public void theta_join() throws Exception {
+        // given - 유저이름이 teamA, B, C
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        // when
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member, team)
+                .where(member.username.eq(team.name))
+                .fetch();
+
+        // then
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("teamA", "teamB");
+    }
+
 }
