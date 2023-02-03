84e301ff4ab9af52c92a8e072d10ed01e62dc744
Griotold <gotjd9773@naver.com>
2023-02-03 12:38:10 +0900

    기본 문법 - 조인 - on절

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -260,4 +260,49 @@ public class QuerydslBasicTest {
                 .containsExactly("teamA", "teamB");
     }
 
+    /**
+     * 조인 - on절
+     * 회원과 팀을 조인하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회(레프트 조인)
+     * JPQL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     * */
+    @Test
+    public void join_on_filtering() throws Exception {
+
+        // when
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(member.team, team)
+                .on(team.name.eq("teamA"))
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+
+        // then
+    }
+
+    /**
+     * 연관관계까 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 대상 외부조인
+     * */
+    @Test
+    public void join_on_no_relation() throws Exception {
+        // given - 유저이름이 teamA, B, C
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        // when
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .fetch();
+
+        // then
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
 }
